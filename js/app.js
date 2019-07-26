9c530f3d7223c46251aa5b657a13813732ed6ab5
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-25 18:17:29 -1000

    finished, ready for review

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//ex1
+
 let person4Name = document.querySelector("#person4Name");
 let person4HomeWorld = document.querySelector("#person4HomeWorld");
 
@@ -24,6 +26,7 @@ oReq.addEventListener("load", reqListener);
 oReq.open("GET", "https://swapi.co/api/people/4");
 oReq.send();
 
+//ex2
 let person14Name = document.querySelector("#person14Name");
 let person14Species = document.querySelector("#person14Species");
 
@@ -32,7 +35,6 @@ const pReq = new XMLHttpRequest();
 function reqListener2() {
   let obj = JSON.parse(this.responseText);
   person14Name.innerHTML = obj.name;
-  console.log(obj);
 
   qReq = new XMLHttpRequest();
 
@@ -49,3 +51,55 @@ function reqListener2() {
 pReq.addEventListener("load", reqListener2);
 pReq.open("GET", "https://swapi.co/api/people/14");
 pReq.send();
+
+//ex3
+
+const rReq = new XMLHttpRequest();
+
+function rReqListener() {
+  let obj = JSON.parse(this.responseText);
+  let filmList = document.querySelector("#filmList");
+  //creates the list of films
+  for (let i = 0; i < obj.results.length; i++) {
+    let film = document.createElement("li");
+    film.className = "film";
+
+    let filmTitle = document.createElement("h2");
+    filmTitle.className = "filmTitle";
+    filmTitle.innerHTML = obj.results[i].title;
+    film.appendChild(filmTitle);
+
+    let filmPlanets = document.createElement("ul");
+    filmPlanets.className = "filmPlanets";
+
+    //creates the list of planets
+    for (let j = 0; j < obj.results[i].planets.length; j++) {
+      let sReq = new XMLHttpRequest();
+      function sReqListener() {
+        let newObj = JSON.parse(this.responseText);
+        let planet = document.createElement("li");
+        planet.className = "planet";
+        let planetName = document.createElement("h4");
+        planetName.className = "planetName";
+        planetName.innerHTML = newObj.name;
+        planet.appendChild(planetName);
+        filmPlanets.appendChild(planet);
+      }
+
+      sReq.addEventListener("load", sReqListener);
+      sReq.open("GET", obj.results[i].planets[j]);
+      sReq.send();
+    }
+
+    let randomH3 = document.createElement("h3");
+    randomH3.innerHTML = "Planets";
+    film.appendChild(randomH3);
+    film.appendChild(filmPlanets);
+
+    filmList.appendChild(film);
+  }
+}
+
+rReq.addEventListener("load", rReqListener);
+rReq.open("GET", "https://swapi.co/api/films/");
+rReq.send();
